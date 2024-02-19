2954c2428903c6ff8335f8d233622017f2916159
Paulo Telles <ptelles040@gmail.com>
2024-02-19 13:25:56 -0300

    testando script

---
+++ src/app/shared/pipes/tranformedNameCityLengh.pipe.ts
@@ -0,0 +1,15 @@
+// import { Pipe, PipeTransform } from "@angular/core";
+// import { query } from "express";
+
+// @Pipe({
+//   name: 'trasnformSizedLengh',
+//   standalone: true
+// })
+// export class TrasnformedSizedLengh implements PipeTransform{
+//   transform(value: string | undefined) {
+//     if (value && value.length > 10) {
+//       return value.
+//     }
+//   }
+
+// }
